5457aa5886f74d4e3134bd8b0945cd5299c47bc4
Evan <>
2025-09-07 09:35:39 -0700

    remove unused imports

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,9 @@
-import json
 import logging
 import discord
 from discord.ext import commands
-from discord import app_commands
-from bot.settings import config 
+from bot.settings import config
 
-from bot.utils.logging_utils import (
-    setup_logging,
-    inject_log_context,
-    log_time
-)
+from bot.utils.logging_utils import setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -17,10 +11,12 @@ logger = logging.getLogger(__name__)
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     logger.info(f"Bot is ready. Logged in as {bot.user} (ID: {bot.user.id})")
+
 
 @bot.event
 async def setup_hook():
@@ -28,5 +24,6 @@ async def setup_hook():
     await bot.load_extension("bot.commands.agent")
     await bot.load_extension("bot.commands.show_cards")
     await bot.load_extension("bot.commands.trade_card")
+
 
 bot.run(config.discord_bot_token)
